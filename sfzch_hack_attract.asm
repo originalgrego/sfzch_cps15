f78b52a390cec485bb1caf09e7ab2d1d50f28ce8
BHDN-Rotwang <piranha.amusements@gmail.com>
2020-12-05 23:55:27 -0800

    Fixed QSound attract mode screen

--- sfzch_hack_attract.asm
+++ sfzch_hack_attract.asm
@@ -14,4 +14,22 @@
  org $19CA
 	jmp hijack_attract_04
 	
+; ================================
+;   Q SOUND JINGLE
+; ================================
+
+ org $11C16		; override pointer to QSound jingle queue code
+	dc.w	$164
+
+; I have no idea what this chunk of code does, but it's in the way
+; of getting the QSound jingle to play and it's also small and easily
+; relocatable. Adding redirect at its start so I can replace the rest
+; of it with a hijack for playing the QSound jingle.
+; The original QSound jingle queue code was replaced with just a RTS
+; so it wasn't long enough to hijack in the conventional way.
+
+ org $11D6E
+	jmp hijack_attract_redirect		
 	
+ org $11D76
+	jmp hijack_attract_qsmusic
--- sfzch_hack_attract_hijacks.asm
+++ sfzch_hack_attract_hijacks.asm
@@ -9,13 +9,16 @@ jtl_hijack_attract_04:
 	
 hijack_attract_04_00:
 	addq.w  #2, ($4,A5)
-	move.w  #$f0, ($6,A5)
-	moveq   #$2d, D0
-	lea     $90c000.l, A1
-	jsr     $19EF2.l
-	moveq   #$2e, D0
-	lea     $90c400.l, A1
-	jsr     $19EF2.l
+	move.w  #$F0, ($6,A5) ; default #$F0
+;	moveq   #$0, D0
+;	lea     $90c000.l, A1
+;	jsr     $19EF2.l
+;	moveq   #$2e, D0	; 2e
+;	lea     $90c400.l, A1
+;	jsr     $19EF2.l
+
+	jsr		draw_qsound_logo
+
 	moveq   #$55, D0
 	jsr     $F1CA.l
 	lea     $908864.l, A1
@@ -29,6 +32,9 @@ hijack_attract_04_00:
 	jsr     $F754.l
 	moveq   #$4, D0
 	jsr     $11BFC.l
+	
+	jsr 	copy_qsound_palette
+	
 	move.w  #$2, D0
 	move.w  #$1, D1
 	jmp     $1720.l
@@ -52,3 +58,60 @@ hijack_attract_08:
 	
 ; ================================
 ; ================================
+
+hijack_attract_redirect:
+	jsr 	$11450
+	moveq	#$30, D1
+	jmp		$114B4
+
+; ================================
+; ================================
+
+hijack_attract_qsmusic:
+	moveq   #$22, D1
+	moveq   #$0, D2
+	moveq   #$0, D3
+	jsr     Hijack_Add_Audio_Command_To_Fifo
+	moveq   #$23, D1
+	move.l  #$11000, D2
+	move.l  #$80, D3
+	jmp     $114B4.l
+	
+; ================================
+; ================================
+
+draw_qsound_logo:
+	
+	moveq	#0, D0
+	move.w	#$180, D0
+	movea.l	#qsound_logo, A0
+	movea.l #$90C000, A1
+	
+draw_qsound_logo_loop:
+	move.l (A0)+, (A1)+
+	dbf		D0, draw_qsound_logo_loop
+	
+	rts
+	
+; ================================
+; ================================
+	
+copy_qsound_palette:
+	moveq	#0, D0
+	move.b	#$60, D0
+	movea.l	#qsound_logo_palette, A0
+	movea.l #$914800, A1
+	
+copy_qsound_logo_palette_loop:
+	move.l (A0)+, (A1)+
+	dbf		D0, copy_qsound_logo_palette_loop
+	
+	rts
+	
+; ================================
+	
+qsound_logo:
+ incbin "scroll2_qsoundlogo.bin"
+ 
+qsound_logo_palette:
+ incbin "palette_qsoundlogo.bin"
